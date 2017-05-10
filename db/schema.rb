0826fbd46d2c58688e9ff4b3e7e6514a2ace6812
Britton Baumann <brittonbaumann@gmail.com>
2017-05-10 10:32:44 -0500

    Created a topics controller and edited the view pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420161558) do
+ActiveRecord::Schema.define(version: 20170509153646) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20170420161558) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -41,6 +43,14 @@ ActiveRecord::Schema.define(version: 20170420161558) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
