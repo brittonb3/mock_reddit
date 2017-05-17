e09fa7602fe426da5c88000f80130352cbb83302
Britton Baumann <brittonbaumann@gmail.com>
2017-05-17 10:12:50 -0500

    Created a user model

--- Gemfile
+++ Gemfile
@@ -59,3 +59,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap-sass'
+
+# Used for encrypting User passwords
+ gem 'bcrypt'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509153646) do
+ActiveRecord::Schema.define(version: 20170517150151) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -51,6 +51,14 @@ ActiveRecord::Schema.define(version: 20170509153646) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
