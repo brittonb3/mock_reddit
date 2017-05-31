d2d1b53b772833d06c8d0c375d54266e543fc027
Britton Baumann <brittonbaumann@gmail.com>
2017-05-31 10:23:59 -0500

    associated posts and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Topics
  15.times do
    Topic.create!(
@@ -19,6 +29,7 @@ require 'random_data'
 # Create Posts
 50.times do
   Post.create!(
+  user:   users.sample,
   topic: topics.sample,
   title: RandomData.random_sentence,
   body: RandomData.random_paragraph
@@ -33,7 +44,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+ user.update_attributes!(
+   email: 'kicker834@yahoo.com', 
+   password: 'helloworld'
+ )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
