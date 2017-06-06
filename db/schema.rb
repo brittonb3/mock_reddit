5c5c95ff0056197af11b8662ab44abb58d12880e
Britton Baumann <brittonbaumann@gmail.com>
2017-06-06 10:43:57 -0500

    created labels for topics and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170605141941) do
+ActiveRecord::Schema.define(version: 20170606151507) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -28,6 +28,22 @@ ActiveRecord::Schema.define(version: 20170605141941) do
     t.integer  "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "labelings", force: :cascade do |t|
+    t.integer  "label_id"
+    t.string   "labelable_type"
+    t.integer  "labelable_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["label_id"], name: "index_labelings_on_label_id"
+    t.index ["labelable_type", "labelable_id"], name: "index_labelings_on_labelable_type_and_labelable_id"
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
