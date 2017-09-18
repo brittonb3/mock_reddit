e854a33e01ac88b95284e7282f86d2472ba0d4ea
Britton Baumann <brittonbaumann@gmail.com>
2017-09-18 11:30:01 -0500

    created an api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912211630) do
+ActiveRecord::Schema.define(version: 20170918161545) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20170912211630) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
